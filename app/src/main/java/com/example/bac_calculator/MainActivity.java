69c5bd11b014a57d542d1d6c05ef387fd96298d8
batesjernigan <bates1012@aol.com>
2015-09-05 13:25:56 -0400

    finish app

--- app/src/main/java/com/example/bac_calculator/MainActivity.java
+++ app/src/main/java/com/example/bac_calculator/MainActivity.java
@@ -1,3 +1,10 @@
+/*
+ * Assignment #: HW 1
+ * MainActivity.java
+ * authors: Thurman Bates Jernigan, Arjun Kabballi Srinivasa, Tempestt Swinson
+*/
+
+
 package com.example.bac_calculator;
 
 import android.graphics.Color;
@@ -14,10 +21,11 @@ import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private double r, A, W, num, deno, ounce, sliderDoubleValue, totalBAC;
+    private double ounce, sliderDoubleValue, totalBAC;
     private int addDrinkClickCount = 0, saveClickCount;
     private boolean flag;
 
@@ -29,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     private SeekBar sb;
     private Switch genderSwitch;
     private TextView statusTextView, bacTextView, alcoholTextView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         rb = (RadioButton) findViewById(R.id.radio_1oz);
         rg = (RadioGroup) findViewById(R.id.radio_group);
 
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
         sb = (SeekBar) findViewById(R.id.seekBar);
 
         genderSwitch = (Switch) findViewById(R.id.Gender_switch);
@@ -56,26 +66,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 flag = isChecked;
-                if(flag){
-                    genderSwitch.setTextOn("on text");
-                } else {
-                    genderSwitch.setTextOff("off text");
-                }
             }
         });
 
         save_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                saveClickCount = 1;
-                weigh_val = weightValue.getText().toString();
+            saveClickCount = 1;
+            weigh_val = weightValue.getText().toString();
 
-                if(weigh_val.equals("")) {
-                    statusTextView.setText(getString(R.string.weightLabelPostError));
-                }  else {
-                    totalBAC = calculateBAC();
-                    changeFields();
-                }
+            if(weigh_val.equals("")) {
+                statusTextView.setText(getString(R.string.weightLabelPostError));
+            }  else {
+                totalBAC = calculateBAC();
+                changeFields();
+            }
             }
         });
 
@@ -83,15 +88,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
 
-                RadioButton r = (RadioButton) findViewById(checkedId);
-                rvalue = r.getText().toString();
+            RadioButton r = (RadioButton) findViewById(checkedId);
+            rvalue = r.getText().toString();
 
-                ounce = 1.0; // defaults to 1.0
-                if(rvalue.equals(getString(R.string.radio5oz))) {
-                    ounce = 5.0;
-                } else if(rvalue.equals(getString(R.string.radio12oz))) {
-                    ounce = 12.0;
-                }
+            ounce = 1.0; // defaults to 1.0
+            if(rvalue.equals(getString(R.string.radio5oz))) {
+                ounce = 5.0;
+            } else if(rvalue.equals(getString(R.string.radio12oz))) {
+                ounce = 12.0;
+            }
             }
         });
 
@@ -100,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 seekBar.setMax(25);
-                alcoholTextView.setText(progress + "");
+                alcoholTextView.setText(progress+"");
             }
 
             @Override
@@ -113,64 +118,74 @@ public class MainActivity extends AppCompatActivity {
         add_drink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addDrinkClickCount ++;
-                weigh_val = weightValue.getText().toString();
+            addDrinkClickCount ++;
+            weigh_val = weightValue.getText().toString();
 
-                if (weigh_val.equals("")) {
-                    statusTextView.setError(getString(R.string.weightLabelPostError));
-                    statusTextView.setText(getString(R.string.weightLabelPostError));
-                } else {
-                    if (addDrinkClickCount == 1 && saveClickCount == 0 ) {
-                        totalBAC = calculateBAC();
-                        changeFields();
-                    } else if(saveClickCount > 0 || addDrinkClickCount > 2) {
-                        totalBAC += calculateBAC();
-                        changeFields();
-                    }
-
+            if (weigh_val.equals("")) {
+                statusTextView.setError(getString(R.string.weightLabelPostError));
+                statusTextView.setText(getString(R.string.weightLabelPostError));
+            } else {
+                if(saveClickCount == 0) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.toastErrorMessage),
+                        Toast.LENGTH_LONG).show();
+                } else if (addDrinkClickCount == 1) {
+                    totalBAC = calculateBAC();
+                    changeFields();
+                } else if(saveClickCount > 0 || addDrinkClickCount > 2) {
+                    totalBAC += calculateBAC();
+                    changeFields();
                 }
+
+            }
             }
         });
 
         resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                weigh_val = "";
-                gender_value = "";
+            weigh_val = "";
+            gender_value = "";
+            addDrinkClickCount = 0;
+            saveClickCount = 0;
 
-                save_btn.setEnabled(true);
-                add_drink.setEnabled(true);
+            save_btn.setEnabled(true);
+            add_drink.setEnabled(true);
 
-                sb.setProgress(Integer.parseInt(getString(R.string.alcohol_value)));
-                rb.setChecked(true);
+            sb.setProgress(Integer.parseInt(getString(R.string.alcohol_value)));
+            rb.setChecked(true);
 
-                weightValue.setText("");
-                genderSwitch.setChecked(false);
+            weightValue.setText("");
+            genderSwitch.setChecked(false);
 
-                statusTextView.setText(getString(R.string.statusSafe));
-                bacTextView.setText(getString(R.string.initialBacValue));
-                totalBAC = 0.00;
+            statusTextView.setText(getString(R.string.statusSafe));
+            statusTextView.setBackgroundColor(Color.GREEN);
+
+            bacTextView.setText(getString(R.string.initialBacValue));
+            progressBar.setProgress(0);
+            totalBAC = 0.00;
             }
         });
     }
 
+    // calculates and returns BAC
     protected double calculateBAC() {
+        double genderConstant, alcoholConsumed, weightInteger;
+        final double CONSTANT_514 = 5.14, CONSTANT_FOR_MEN = 0.73, CONSTANT_FOR_WOMEN = 0.66;
+
         gender_value = flag ? "Male" : "Female";
-        Log.d("demo", "gender value: " + gender_value);
-
-
-        r = (gender_value == "Male") ? 0.73 : 0.66;
+        genderConstant = (gender_value.equals("Male")) ? CONSTANT_FOR_MEN : CONSTANT_FOR_WOMEN;
         sliderDoubleValue = Double.parseDouble(alcoholTextView.getText().toString());
 
-        A = ounce * sliderDoubleValue / 100;
-        W = Integer.parseInt(weigh_val);
+        alcoholConsumed = ounce * sliderDoubleValue / 100;
+        weightInteger = Integer.parseInt(weigh_val);
 
-        num = A * 5.14;
-        deno = W * r;
-        return num/deno;
+        return (alcoholConsumed * CONSTANT_514)/(weightInteger * genderConstant);
     }
 
+    // changes fields to approporate values/stylings
     public void changeFields() {
+        int integerProgress = (int) (totalBAC * 100);
+        progressBar.setProgress(integerProgress);
         bacTextView.setText((String.format("%.2f", totalBAC)));
         statusTextView.setTextColor(Color.WHITE);
 
@@ -179,6 +194,7 @@ public class MainActivity extends AppCompatActivity {
             statusTextView.setBackgroundColor(Color.GREEN);
         } else if(totalBAC > 0.08 && totalBAC < 0.20) {
             statusTextView.setText(getString(R.string.statusWarning));
+            statusTextView.setTextColor(Color.BLACK);
             statusTextView.setBackgroundColor(Color.YELLOW);
         } else if(totalBAC >= 0.20 && totalBAC < 0.25) {
             statusTextView.setText(getString(R.string.statusDanger));
@@ -189,8 +205,8 @@ public class MainActivity extends AppCompatActivity {
             save_btn.setEnabled(false);
             add_drink.setEnabled(false);
 
-            Toast.makeText(getApplicationContext(), getString(R.string.toastMessage),
-                    Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), getString(R.string.toastGoHomeMessage),
+                Toast.LENGTH_LONG).show();
         }
     }
 }
